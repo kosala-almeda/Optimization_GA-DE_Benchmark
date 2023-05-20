2cdded0c7245f6a19bdd0fbd3ea52a937fdf45af
Kosala Almeda <kosala86@hotmail.com>
2023-05-20 15:57:02 -0400

    DE 2

--- DifferentialEvolution.m
+++ DifferentialEvolution.m
@@ -1,11 +1,12 @@
 classdef DifferentialEvolution
     
-    properties (Constant)
+    properties(Constant)
         % Differential Evolution parameters
         POPULATION_SIZE = 100;
         MAX_GENERATIONS = 1000;
         MAX_FITNESS_EVALUATIONS = 3000;
         CROSSOVER_RATE = 0.9;
+        MUTATION_RATE = 0.5;
         SCALE_FACTOR = 0.5;
         DIMENSION_RANGE = [-10 10];
     end
@@ -40,114 +41,123 @@ classdef DifferentialEvolution
                 log = false;
             end
             % Run the Differential Evolution algorithm
-            obj = obj.initializePopulation();
-            obj = obj.evaluatePopulation();
-            
+            obj = obj.runSingleStep(log);
             bestIndividuals = [];
             bestFitness = [];
-            
+            % evolve and evaluate
             while obj.generation < obj.MAX_GENERATIONS
+                % show the best fitness
                 if obj.numFitnessCalls >= obj.MAX_FITNESS_EVALUATIONS
                     break;
                 end
-                obj = obj.evolvePopulation();
-                obj = obj.evaluatePopulation();
-                
-                if log
-                    fprintf('Generation: %d, NFC: %d\n', obj.generation, obj.numFitnessCalls);
-                    fprintf('Best individual:\n');
-                    fprintf('\tgenes: ');
-                    fprintf('%f ', obj.bestIndividual);
-                    fprintf('\n\tfitness: %f', obj.bestFitness);
-                    fprintf('\n');
-                    fprintf('----------------------------------------\n');
-                end
-                
+                obj = obj.runSingleStep(log);
                 bestIndividuals = [bestIndividuals; obj.bestIndividual];
                 bestFitness = [bestFitness; obj.bestFitness];
             end
         end
         
-        function obj = evolvePopulation(obj)
-            obj.generation = obj.generation + 1;
-            newPopulation = obj.population;
-            
-            for i = 1:obj.POPULATION_SIZE
-                targetIdx = i;
-                
-                % Select three distinct individuals for mutation
-                r = randperm(obj.POPULATION_SIZE, 3);
-                a = obj.population(r(1), :);
-                b = obj.population(r(2), :);
-                c = obj.population(r(3), :);
-                
-                % Perform mutation
-                mutated = a + obj.SCALE_FACTOR * (b - c);
-                mutated = obj.clipToBounds(mutated);
-                
-                % Perform crossover
-                crossed = obj.crossover(obj.population(targetIdx, :), mutated);
-                
-                % Select between the mutated and original individual
-                trial = obj.selectTrialIndividual(obj.population(targetIdx, :), crossed);
-                
-                % Replace the target individual if the trial is better
-                if obj.fitness(trial) < obj.fitness(obj.population(targetIdx, :))
-                    newPopulation(targetIdx, :) = trial;
-                end
+        % Run a single step of the Differential Evolution algorithm
+        function obj = runSingleStep(obj, log)
+            if nargin < 2
+                log = false;
             end
-            
-            obj.population = newPopulation;
-        end
-        
-        function individual = crossover(obj, target, mutated)
-            individual = target;
-            
-            for j = 1:obj.numDimensions
-                if rand < obj.CROSSOVER_RATE
-                    individual(j) = mutated(j);
-                end
+            % Run a single step of the Differential Evolution algorithm
+            if obj.generation == 0
+                obj = obj.initializePopulation();
+                obj = obj.evaluatePopulation();
+            else
+                obj = obj.evolvePopulation();
+                obj = obj.evaluatePopulation();
+            end
+            if log
+                fprintf('Generation: %d, NFC: %d\n', obj.generation, obj.numFitnessCalls);
+                fprintf('Best individual:\n');
+                fprintf('\tGenes: ');
+                fprintf('%f ', obj.bestIndividual);
+                fprintf('\n\tFitness: %f', obj.bestFitness);
+                fprintf('\n');
+                fprintf('----------------------------------------\n');
             end
         end
         
-        function trial = selectTrialIndividual(obj, target, crossed)
-            trial = target;
-            
-            j = randi(obj.numDimensions);
-            for k = 1:obj.numDimensions
-                if k == j || rand < obj.CROSSOVER_RATE
-                    trial(k) = crossed(k);
-                end
-            end
-        end
-        
+        % Initialize the population randomly
         function obj = initializePopulation(obj)
-            obj.population = rand(obj.POPULATION_SIZE, obj.numDimensions) * ...
-                (obj.DIMENSION_RANGE(2) - obj.DIMENSION_RANGE(1)) + obj.DIMENSION_RANGE(1);
-            
+            % Initialize the population
+            obj.population = rand(obj.POPULATION_SIZE, obj.numDimensions) ...
+                * (obj.DIMENSION_RANGE(2) - obj.DIMENSION_RANGE(1)) ...
+                + obj.DIMENSION_RANGE(1);
             obj.generation = 1;
         end
         
+        % Evaluate and sort the population by fitness
         function obj = evaluatePopulation(obj)
+            % Evaluate the population
             popFitness = zeros(1, obj.POPULATION_SIZE);
-            
             for i = 1:obj.POPULATION_SIZE
                 popFitness(i) = obj.fitness(obj.population(i, :));
                 obj.numFitnessCalls = obj.numFitnessCalls + 1;
             end
-            
-            [obj.bestFitness, bestIdx] = min(popFitness);
-            obj.bestIndividual = obj.population(bestIdx, :);
+            % Sort the population by fitness
+            [popFitness, index] = sort(popFitness);
+            obj.population = obj.population(index, :);
+            % Update the best fitness and individual
+            obj.bestFitness = popFitness(1);
+            obj.bestIndividual = obj.population(1, :);
         end
         
+        % Evolve the population
+        function obj = evolvePopulation(obj)
+            % Evolve the population
+            obj.generation = obj.generation + 1;
+            newPopulation = obj.population;
+            for i = 1:obj.POPULATION_SIZE
+                % Select parents
+                parents = obj.selectParents(i);
+                parent1 = parents(1, :);
+                parent2 = parents(2, :);
+                parent3 = parents(3, :);
+                % Mutate
+                mutant = obj.mutate(parent1, parent2, parent3);
+                % Crossover
+                trial = obj.crossover(obj.population(i, :), mutant);
+                % Select between the trial and the target
+                target = obj.population(i, :);
+                if obj.fitness(trial) <= obj.fitness(target)
+                    newPopulation(i, :) = trial;
+                end
+            end
+            obj.population = newPopulation;
+        end
+        
+        % Select parents for mutation
+        function parents = selectParents(obj, targetIndex)
+            % Select parents for mutation (rand/1/bin)
+            indices = 1:obj.POPULATION_SIZE;
+            indices(targetIndex) = [];
+            randomIndices = randperm(obj.POPULATION_SIZE - 1, 3);
+            parents = obj.population(indices(randomIndices), :);
+        end
+        
+        % Mutate the target individual
+        function mutant = mutate(obj, parent1, parent2, parent3)
+            % Mutate the target individual (rand/1/bin)
+            difference = obj.SCALE_FACTOR * (parent2 - parent3);
+            mutant = parent1 + difference;
+        end
+        
+        % Perform crossover between the target and mutant individuals
+        function trial = crossover(obj, target, mutant)
+            % Perform crossover between the target and mutant individuals
+            mask = rand(size(target)) < obj.CROSSOVER_RATE;
+            trial = target;
+            trial(mask) = mutant(mask);
+        end
+        
+        % Evaluate the fitness of an individual
         function fitness = fitness(obj, individual)
+            % Evaluate the fitness of an individual
             fitness = obj.objectiveFunction(individual);
         end
         
-        function clipped = clipToBounds(obj, individual)
-            clipped = min(max(individual, obj.DIMENSION_RANGE(1)), obj.DIMENSION_RANGE(2));
-        end
-        
     end
-    
 end
